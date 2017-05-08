2803c201cd5700764d330406b4759b46794d80ad
acantepie <adrien.cantepie@gmail.com>
2017-05-08 16:04:14 +0200

    Layout

---
+++ symfony/src/AdminLTE/AdminBundle/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: acantepie
+ * Date: 08/05/17
+ * Time: 14:51
+ */
+
+namespace AdminLTE\AdminBundle\Controller;
+
+use Umbrella\AdminBundle\Controller\Lib\BaseController;
+
+/**
+ * Class UserController
+ * @package AdminLTE\AdminBundle\Controller
+ */
+class UserController extends BaseController
+{
+    
+    public function listAction()
+    {
+        
+    }
+
+}
